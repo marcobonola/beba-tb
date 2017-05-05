c4b449ce5bf5d90510606654873017451ac4a710
mbonola <marlonsH501O>
2017-05-05 15:21:32 +0200

    fix vari tra cui 4 secondi iniziali e grafica

--- src/app/realtime/realtime.controller.js
+++ src/app/realtime/realtime.controller.js
@@ -1,75 +1,104 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular
-    .module('angularProjects')
-    .controller('RealTimeController', RealTimeController);
+    angular
+      .module('angularProjects')
+      .controller('RealTimeController', RealTimeController);
 
-  /** @ngInject */
-  function RealTimeController($http) {
-    var vm = this;
+    /** @ngInject */
+    function RealTimeController($http) {
+      var vm = this;
 
-    vm.options = {
-      chart: {
-        type: 'discreteBarChart',
-        height: 450,
-        width: 1000,
-        margin : {
-          top: 20,
-          right: 20,
-          bottom: 50,
-          left: 55
-        },
-        x: function(d){return d.label;},
-        y: function(d){return d.value + (1e-10);},
-        showValues: true,
-        valueFormat: function(d){
-          return d3.format(',.4f')(d);
-        },
-        duration: 500,
-        xAxis: {
-          axisLabel: 'X Axis'
-        },
-        yAxis: {
-          axisLabel: 'Y Axis',
-          axisLabelDistance: -10
+       $http({
+         method: 'GET',
+         url: 'http://beba-demo-be.netgroup.uniroma2.it/realtime'
+       }).then(function successCallback(response) {
+         vm.data[0].values = [];
+         for (var i = 0; i < response.data["samples"].length; i++) {
+           vm.data[0].values.push({
+             label: response.data["samples"][i]["ip"],
+             value: response.data["samples"][i]["pps"]
+           });
+         }
+       });
+
+
+
+
+      vm.options = {
+        chart: {
+          type: 'discreteBarChart',
+          height: 450,
+          width: 1000,
+          margin: {
+            top: 20,
+            right: 20,
+            bottom: 50,
+            left: 70
+          },
+          x: function (d) {
+            return d.label;
+          },
+          y: function (d) {
+            return d.value;
+          },
+          showValues: true,
+
+          valueFormat: function (d) {
+            return d3.format('d')(d);
+          },
+          duration: 500,
+          xAxis: {
+            axisLabel: 'flows'
+          },
+          yAxis: {
+            axisLabel: 'pps',
+            tickFormat: function (d) {
+              return d3.format('d')(d)
+            },
+          }
         }
-      }
-    };
+      };
 
-    vm.data = [
-      {
-        key: "Cumulative Return",
-        values: []
-      }
-    ]
-
-
-    setInterval(function () {
-
-      $http({
-        method: 'GET',
-        url: 'http://beba-demo-be.netgroup.uniroma2.it/realtime'
-      }).then(function successCallback(response) {
-        if (response.data["samples"].length === 0) {return};
-
-        // this callback will be called asynchronously
-        // when the response is available
-        console.log("GET OK");
-        vm.data[0].values=[];
-
-        for (var i = 0; i < response.data["samples"].length; i++) {
-          vm.data[0].values.push({label:response.data["samples"][i]["ip"], value:response.data["samples"][i]["pps"]});
+      vm.data = [
+        {
+          key: "",
+          values: []
         }
+      ]
 
-      }, function errorCallback(response) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-        console.log("GET ERROR");
-      });
 
-  }, 4000);
+      setInterval(function () {
 
-    return vm;
-  }
-})();
+        $http({
+          method: 'GET',
+          url: 'http://beba-demo-be.netgroup.uniroma2.it/realtime'
+        }).then(function successCallback(response) {
+          if (response.data["samples"].length === 0) {
+            return
+          }
+          ;
+
+          // this callback will be called asynchronously
+          // when the response is available
+          console.log("GET OK");
+          vm.data[0].values = [];
+
+          for (var i = 0; i < response.data["samples"].length; i++) {
+            vm.data[0].values.push({
+              label: response.data["samples"][i]["ip"],
+              value: response.data["samples"][i]["pps"]
+            });
+          }
+
+        }, function errorCallback(response) {
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+          console.log("GET ERROR");
+        });
+
+      }, 4000);
+
+      return vm;
+    }
+  })();
